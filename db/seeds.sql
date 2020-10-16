658ad017ca0218be7c1012602a01349442723dab
Aeryk Marrero <aerykmarrero@yahoo.com>
2020-10-15 19:33:50 -0500

    Revert "adding the database into the repo"

--- db/seeds.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO bills (subs_name,subs_cost,trial_period,due_date) VALUES ('Netflix', '14.99', true, '2020-10-14');
-INSERT INTO bills (subs_name,subs_cost,due_date) VALUES ('Hulu', '5.99', '2020-10-22');
-INSERT INTO bills (subs_name,subs_cost,due_date) VALUES ('Spotify', '9.99', '2020-11-01');
-INSERT INTO bills (subs_name,subs_cost,due_date) VALUES ('Amazon', '12.99', '2020-11-03');
